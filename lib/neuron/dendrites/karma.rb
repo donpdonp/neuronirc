14f613fbc7f9b098ec2f1ac9b0e0a881d0dc148d
Don Park <don@donpark.org>
2011-12-11 15:24:09 -0800

    limit zee karma

--- lib/neuron/dendrites/karma.rb
+++ lib/neuron/dendrites/karma.rb
@@ -13,10 +13,12 @@ require 'json'
       message = JSON.parse(json)
       puts "Heard #{message}"
       if message["target"][0] == '#' && message["type"] == "emessage" && message["to_me"] == "true"
-        expr = message["message"].match(/!?karma ?(\w+)?/)
+        expr = message["message"].match(/!?karma ?([-+\d]+)?/)
         if expr
           count = expr[1] ? expr[1].to_i : 1
           k = count > 0 ? "++" : "--"
+          count = 5 if count.abs > 5
+          count = -5 if count.abs < -5
           count.abs.times do
             msg = "#{message["nick"]}#{k}"
             predis.publish :say, {"command" => "say", 
