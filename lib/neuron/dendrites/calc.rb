8ac498d9de2ec5e551ca8c2e9536100eb24c8f82
Don Park <don@donpark.org>
2012-03-29 14:10:09 -0700

    solve with wolfram alpha

--- lib/neuron/dendrites/calc.rb
+++ lib/neuron/dendrites/calc.rb
@@ -1,6 +1,12 @@
 #!/usr/local/ruby/1.9.2-p136/bin/ruby
 require 'redis'
 require 'json'
+require 'httparty'
+require 'nokogiri'
+
+STDOUT.sync = true
+BASE_DIR = File.expand_path(File.join(File.dirname(__FILE__),"."))
+SETTINGS = JSON.load(File.open(File.join(BASE_DIR,"../../../settings.json")))
 
   STDOUT.sync = true
   redis = Redis.new
@@ -12,7 +18,7 @@ require 'json'
     on.message do |channel, json|
       message = JSON.parse(json)
       puts "Heard #{message}"
-      if message["target"][0] == '#' && message["type"] == "emessage"
+      if message["target"] && message["target"][0] == '#' && message.has_key?("type") && message["type"] == "emessage"
         expr = message["message"].match(/!?calc\s+([0-9\.\^*+-\/()^<>= ]+)/)
         if expr
           answer = eval expr[1] rescue nil
@@ -21,9 +27,30 @@ require 'json'
           if message["to_me"] == "true"
             msg = "#{message["nick"]}: "+msg
           end
-          predis.publish :say, {"command" => "say", 
-                                "target" => message["target"], 
+          predis.publish :say, {"command" => "say",
+                                "target" => message["target"],
                                 "message" => msg}.to_json unless answer.nil?
+        end
+
+        solve = message["message"].match(/!?solve\s+(.*)/)
+        if solve && solve.captures.size > 0
+          query = solve.captures[0]
+          puts "Wolfram Alpha query: #{query}"
+          answer = HTTParty.get "http://api.wolframalpha.com/v2/query",
+                      :query => {"input" => query,
+                                 "appid" => SETTINGS['wolfram']['appid']}
+          xml = Nokogiri::XML(answer.body)
+          result = xml.xpath("/queryresult/pod[@title='Result']/subpod/plaintext")
+          solution = xml.xpath("/queryresult/pod[@title='Solution']/subpod/plaintext")
+          answer = "#{result.text}#{solution.text}"
+          if answer.blank?
+            msg = "cant say"
+          else
+            msg = answer
+          end
+          predis.publish :say, {"command" => "say",
+                                "target" => message["target"],
+                                "message" => msg}.to_json
         end
       end
     end
