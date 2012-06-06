624c3dad9d2cdd23c551d5e1b8a3cccd9708ae32
Don Park <don@donpark.org>
2012-06-06 09:51:24 -0700

    emessage test first

--- lib/neuron/dendrites/stocks.rb
+++ lib/neuron/dendrites/stocks.rb
@@ -14,20 +14,21 @@ require 'csv'
     on.message do |channel, json|
       message = JSON.parse(json)
       puts "Heard #{message}"
-      if message["target"][0] == '#' && message["type"] == "emessage"
+      if message["type"] == "emessage" && message["target"][0] == '#'
         expr = message["message"].match(/^!?(quote|stock)\s+(.+)/)
         if expr
           symbol = expr[2]
           puts "Searching YQL for stock symbol #{symbol}"
-          csv = Faraday.get do|r| 
-            r.url "http://query.yahooapis.com/v1/public/yql", 
-            :q=>"select * from yahoo.finance.quotes where symbol in (#{symbol.to_json})", 
-            :env=>'http://datatables.org/alltables.env', 
+          csv = Faraday.get do|r|
+            r.url "http://query.yahooapis.com/v1/public/yql",
+            :q=>"select * from yahoo.finance.quotes where symbol in (#{symbol.to_json})",
+            :env=>'http://datatables.org/alltables.env',
             :format=>'json'
           end
           msg = ""
           response=JSON.parse(csv.body)
           q = response["query"]["results"]["quote"]
+          puts q.inspect
           msg += ["#{q["Symbol"]} \"#{q["Name"]}\"",
                   "last: $#{q["LastTradePriceOnly"]}",
                   "avg. daily volume: #{q["AverageDailyVolume"]}",
@@ -37,8 +38,8 @@ require 'csv'
             msg = "#{message["nick"]}: #{msg}"
           end
 
-          predis.publish :say, {"command" => "say", 
-                                "target" => message["target"], 
+          predis.publish :say, {"command" => "say",
+                                "target" => message["target"],
                                 "message" => msg}.to_json unless csv.nil?
         end
       end
