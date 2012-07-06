7c9f51e84ae9dafadbf328e564b4c48aceaef563
Don Park <don@donpark.org>
2012-07-06 16:10:24 -0700

    refactor

--- lib/neuron/dendrites/metajs.rb
+++ lib/neuron/dendrites/metajs.rb
@@ -27,75 +27,75 @@ class Metajs
         message = JSON.parse(json)
         puts "Heard #{message}"
         if message["type"] == "emessage"
-          if message["target"][0] == '#'
+          if message["target"] && message["target"][0] == '#'
             funcs = credis.lrange('functions', 0, credis.llen('functions'))
+            funcs.each {|js| exec_js(js) }
 
-            funcs.each do |func|
-              begin
-                puts "parsing: #{func}"
-                func = "(#{func})(#{message["message"].to_json})"
-                puts "parsing2: #{func}"
-                response = v8.eval(func)
-                puts "got: #{response}"
-                if response.to_s.length > 0
-                  @redis.publish :say, {"command" => "say",
-                                        "target" => message["target"],
-                                        "message" => response}.to_json
-                end
-              rescue V8::JSError => e
-                puts "Error: #{e}"
+            match = message["message"].match(/^js (.*)$/)
+            if match
+              case match.captures.first
+              when "wipe"
+                credis.del('functions')
+                msg = "all functions wiped"
                 @redis.publish :say, {"command" => "say",
                                       "target" => message["target"],
-                                      "message" => e.to_s}.to_json
-              end
-            end
-
-            if match = message["message"].match(/^responder wipe$/)
-              credis.del('functions')
-              msg = "all functions wiped"
-              @redis.publish :say, {"command" => "say",
-                                    "target" => message["target"],
-                                    "message" => msg}.to_json
-              return
-            end
-
-            if match = message["message"].match(/^responder (.*)/)
-              begin
-                func = "(#{match.captures.first})()"
-                puts "add parsing: #{func}"
-                response = v8.eval(func)
-                msg = "adding #{match.captures.first}"
-                credis.rpush('functions', match.captures.first)
+                                      "message" => msg}.to_json
+                return
+              when "add"
+                begin
+                  func = "(#{match.captures.first})()"
+                  puts "add parsing: #{func}"
+                  response = v8.eval(func)
+                  msg = "adding #{match.captures.first}"
+                  credis.rpush('functions', match.captures.first)
+                  # run it through the defined functions
+                  # say the result
+                  @redis.publish :say, {"command" => "say",
+                                        "target" => message["target"],
+                                        "message" => msg}.to_json
+                rescue NoMethodError => e
+                  @redis.publish :say, {"command" => "say",
+                                        "target" => message["target"],
+                                        "message" => e.to_s}.to_json
+                rescue V8::JSError => e
+                  @redis.publish :say, {"command" => "say",
+                                        "target" => message["target"],
+                                        "message" => e.to_s}.to_json
+                end
+              when "list"
+                msg = "funcs #{funcs.inspect}"
                 # run it through the defined functions
                 # say the result
                 @redis.publish :say, {"command" => "say",
                                       "target" => message["target"],
                                       "message" => msg}.to_json
-              rescue NoMethodError => e
-                @redis.publish :say, {"command" => "say",
-                                      "target" => message["target"],
-                                      "message" => e.to_s}.to_json
-              rescue V8::JSError => e
-                @redis.publish :say, {"command" => "say",
-                                      "target" => message["target"],
-                                      "message" => e.to_s}.to_json
               end
             end
-
-            if match = message["message"].match(/^responder$/)
-              msg = "funcs #{funcs.inspect}"
-              # run it through the defined functions
-              # say the result
-              @redis.publish :say, {"command" => "say",
-                                    "target" => message["target"],
-                                    "message" => msg}.to_json
-            end
-
           end
         end
       end
     end
   end
+
+  def exec_js(js)
+    begin
+      func = "(#{func})(#{message["message"].to_json})"
+      puts "parsing: #{func}"
+      response = v8.eval(func)
+      puts "got: #{response}"
+      if response.to_s.length > 0
+        @redis.publish :say, {"command" => "say",
+                              "target" => message["target"],
+                              "message" => response}.to_json
+      end
+    rescue V8::JSError => e
+      puts "Error: #{e}"
+      @redis.publish :say, {"command" => "say",
+                            "target" => message["target"],
+                            "message" => e.to_s}.to_json
+    end
+  end
+
 end
 
 class MyHttp
