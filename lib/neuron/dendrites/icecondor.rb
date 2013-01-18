2ae20c5b7dfadc86139a5552cd840bff2f2980dc
Don Park <don@donpark.org>
2013-01-18 14:44:19 -0800

    yea for faye/websocket

--- lib/neuron/dendrites/icecondor.rb
+++ lib/neuron/dendrites/icecondor.rb
@@ -1,11 +1,26 @@
 require 'neuron/dendrite'
+require 'faye/websocket'
+
+STDOUT.sync = true
 
 class IceCondor
   include Neuron::Dendrite
 
   def go
+    EventMachine.run do
+      uri = "wss://api.icecondor.com"
+      puts "IceCondor connecting #{uri}"
+      ws = Faye::WebSocket::Client.new(uri)
+      ws.onopen = lambda do |event|
+
+      end
+      ws.onmessage = lambda do |event|
+        puts event.data
+      end
+    end
+
     on_message do |channel, message|
-      puts "icecondor! #{message}"
+      puts "#{message}"
     end
   end
 end
